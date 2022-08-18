bdafda69b87b896b72221e0e36937bc54f7bab84
cywhale <cyrain@gmail.com>
2022-08-18 11:53:52 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+'use strict'
+import Fastify from 'fastify'
+import Static from '@fastify/static'
+//import fs, { readFileSync } from 'fs'
+import { join } from 'desm'
+import { pipeline } from 'stream'
+
+const startServer = async () => {
+  const PORT = 3000;
+  const fastify = Fastify({
+      trustProxy: true,
+      requestTimeout: 5000,
+      logger: true
+  })
+
+  fastify.register(Static, {
+    root: join(import.meta.url, '..', 'public'),
+    //prefix: '/public/', // optional: default '/'
+  })
+
+  fastify.get('/', function (req, reply) {
+    reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'index.html') directly
+  })
+
+  fastify.get('/test', async function (req, reply) {
+    fastify.log.info("test route")
+    reply.send('Test ok!')
+  })
+
+  fastify.listen({ port: PORT }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+  })
+}
+
+startServer()
+
